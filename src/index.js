80d4a424396fb4c3002c8d925d408041413086f9
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-06-07 15:06:06 +0300

    Fix linter

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import "./styles/style.css";
-import Alltask from "./add.js";
-import { real, displayUI, newTask } from "./reload.js";
-import { read, addButton } from "./addtask.js";
+import './styles/style.css';
+import Alltask from './add.js';
+import { real, displayUI, newTask } from './reload.js';
+import { read, addButton } from './addtask.js';
 
 let tasks = [];
-addButton.addEventListener("click", (e) => {
-  tasks = JSON.parse(localStorage.getItem("list")) || [];
+addButton.addEventListener('click', (e) => {
+  tasks = JSON.parse(localStorage.getItem('list')) || [];
   e.preventDefault();
   const findIndex = tasks.length;
   const task1 = new Alltask(read.value, false, findIndex + 1);
   tasks.push(task1);
   real(task1);
-  read.value = "";
-  localStorage.setItem("list", JSON.stringify(tasks));
+  read.value = '';
+  localStorage.setItem('list', JSON.stringify(tasks));
 });
 
-newTask.addEventListener("click", (e) => {
-  tasks = JSON.parse(localStorage.getItem("list")) || [];
+newTask.addEventListener('click', (e) => {
+  tasks = JSON.parse(localStorage.getItem('list')) || [];
 
-  if (e.target.classList.contains("remove")) {
+  if (e.target.classList.contains('remove')) {
     e.target.parentElement.parentElement.remove();
     const k = parseInt(e.target.id, 10);
     const newArray = tasks.filter((Objects) => Objects.index !== k);
@@ -26,24 +26,24 @@ newTask.addEventListener("click", (e) => {
       newArray[i].index = 1;
       newArray[i].index += i;
     }
-    localStorage.setItem("list", JSON.stringify(newArray));
+    localStorage.setItem('list', JSON.stringify(newArray));
     window.location.reload();
   } else if (e.target.checked) {
     const k = parseInt(e.target.id, 10);
     tasks[k - 1].completed = true;
-    localStorage.setItem("list", JSON.stringify(tasks));
+    localStorage.setItem('list', JSON.stringify(tasks));
   } else {
     const k = parseInt(e.target.id, 10);
     tasks[k - 1].completed = false;
-    localStorage.setItem("list", JSON.stringify(tasks));
+    localStorage.setItem('list', JSON.stringify(tasks));
   }
 });
 
-const clear = document.querySelector(".clear");
-clear.addEventListener("click", (e) => {
-  tasks = JSON.parse(localStorage.getItem("list")) || [];
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', (e) => {
+  tasks = JSON.parse(localStorage.getItem('list')) || [];
 
-  if (e.target.classList.contains("new-task-pro")) {
+  if (e.target.classList.contains('new-task-pro')) {
     e.target.parentElement.parentElement.remove();
   }
 
@@ -53,7 +53,7 @@ clear.addEventListener("click", (e) => {
     clearArrray[i].index = 1;
     clearArrray[i].index += i;
   }
-  localStorage.setItem("list", JSON.stringify(clearArrray));
+  localStorage.setItem('list', JSON.stringify(clearArrray));
   window.location.reload();
 });
 
--- src/reload.js
+++ src/reload.js
@@ -1,14 +1,11 @@
-
 const taskList = document.querySelector('.task-list');
 export const newTask = document.createElement('div');
 const localStorageArray = JSON.parse(localStorage.getItem('list')) || [];
 
-
-
 export const displayUI = () => {
-    localStorageArray.forEach((list) => {
-      newTask.classList.add('new-task');
-      newTask.innerHTML += `<div class='new-task-pro'><input type='checkbox' id ='${list.index}' class='checkbox' name='checkbox'/>
+  localStorageArray.forEach((list) => {
+    newTask.classList.add('new-task');
+    newTask.innerHTML += `<div class='new-task-pro'><input type='checkbox' id ='${list.index}' class='checkbox' name='checkbox'/>
           <div>
           <p class='edit'>${list.description}</p>
           </div>
@@ -17,13 +14,13 @@ export const displayUI = () => {
           <ion-icon name='trash-outline'  class='remove' id ='${list.index}'></ion-icon>
           </div></div>
           `;
-      taskList.append(newTask);
-    });
-  };
-  
-  export const real = (list) => {
-    newTask.classList.add('new-task');
-    newTask.innerHTML += `<div class='new-task-pro'><input type='checkbox' id ='${list.index}' class='checkbox' name='checkbox'/>
+    taskList.append(newTask);
+  });
+};
+
+export const real = (list) => {
+  newTask.classList.add('new-task');
+  newTask.innerHTML += `<div class='new-task-pro'><input type='checkbox' id ='${list.index}' class='checkbox' name='checkbox'/>
         <div>
         <p class='edit' >${list.description}</p>
         </div>
@@ -32,6 +29,5 @@ export const displayUI = () => {
         <ion-icon name='trash-outline' class='remove' id ='${list.index}'></ion-icon>
         </div></div>
         `;
-    taskList.append(newTask);
-  };
-
+  taskList.append(newTask);
+};
